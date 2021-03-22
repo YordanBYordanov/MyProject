7b04b162e8a02ed8c80ad656c51fae15798fab10
YordanBYordanov <dancho.86@abv.bg>
2021-03-22 15:03:00 +0100

    Add Header to client_app

---
+++ client_app/src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import {Component} from 'react';
+import styles from '../Header/Header.module.css';
+import commonStyle from '../Common.module.css';
+
+class Header extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className={[styles.divHeaderWrapper].join(" ")}>
+                <div className={[styles.header, commonStyle.siteWrapper].join(" ")}>
+                    <h1 className={styles.headerTitle}>My Test Website</h1>
+                    <nav className={styles.headerNav}>
+                        <ul className={styles.headerUl}>
+                            <li className={styles.headerLi}><a className={styles.headerA} href="#">Register</a></li>
+                            <li className={styles.headerLi}><a className={styles.headerA} href="#">Login</a></li>
+                            <li className={styles.headerLi}><a className={styles.headerA} href="#">AboutUs</a></li>
+                            <li className={styles.headerLi}><a className={styles.headerA} href="#">ContactUs</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
